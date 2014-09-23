83697bf654cd6573b7a41f416811138a3d12fc1e
Vilhelm Roxling <vilhelm.roxling@gmail.com>
2014-09-23 14:34:50 +0200

    hej

---
+++ assignments/assignment2/orthogonalization.py
@@ -0,0 +1,139 @@
+from    __future__  import division
+from    scipy       import *
+from    matplotlib.pyplot   import *
+
+import numpy as np
+
+class Orthogonalization:
+
+    """
+    It takes as an input the m x n array A (transposed for coding c
+    onvenience) and assume_li which is a boolean(False by default) t
+    elling us if the columns of A can be assumed to be linearly iden
+    pendent or not. 
+    """
+
+    def __init__(self, A, assume_li = False):
+        self.assume_li = assume_li
+        self.A = A
+        [self.n, self.m] = A.shape
+
+    def gramschmidt(self):
+        tol = 1e-12
+        
+        Q = np.zeros((self.n, self.m))
+        Q[0] = self.A[0]/np.linalg.norm(self.A[0])
+        lin_dep_inds = []
+
+        for i in range(1,self.n):
+            f = self.A[i]
+            projvec = zeros(self.m)
+
+            for k in range(i):
+                projvec += np.dot(Q[k],f)*Q[k]        
+        
+            v = f - projvec
+            vn = np.linalg.norm(v)
+
+            if self.assume_li:
+                Q[i] = v/vn
+
+            else:
+                if vn > tol:
+                    Q[i] = v/vn
+                else:
+                    lin_dep_inds.append(i)
+        
+        return delete(Q, lin_dep_inds, axis=0)
+
+    def householder(self, both = True):
+        R = self.A.copy()
+        self.v_list = []
+
+        for i in range(self.n):
+            a = R[i][i:]
+            e = zeros(self.m-i)
+            e[0] = np.linalg.norm(a)
+            v = np.sign(a[0])*e + a
+            v = v/np.linalg.norm(v)
+            self.v_list.append(v)
+            R[i,i:] = -sign(a[0])*e
+            for j in range(1,self.n-i):
+                R[i+j,i:] -= 2*v*np.dot(v, R[i+j,i:])
+            
+        if not both:
+            return R, self.v_list
+        
+        Q = zeros((self.m, self.m))
+        I = eye(self.m)
+    
+        for e, i in zip(I[:], range(self.m)):
+            if self.n-1-i >= 0:
+                k = self.n-1-i
+                e[i:] -= 2*self.v_list[i][0]*self.v_list[i]
+            else:
+                k = 0
+                e[self.n-1:] -= 2*self.v_list[-1][i-(self.n-1)]*self.v_list[-1]
+                
+            for j in range(k+1, self.n):
+                v = self.v_list[self.n-1-j]
+                e[self.n-1-j:] -= 2*np.dot(v, e[self.n-1-j:])*v
+
+            Q[i] = e
+
+        return Q.T, R.T
+            
+    def qdot(self, b):
+        
+        for i in range(self.n):
+            v = self.v_list[self.n-1-i]
+            b[self.n-1-i:] -= 2 * np.dot(v, b[self.n-1-i:])*v
+
+        return b
+
+    def givens(self, both = True):
+        
+        g_list = []
+        R = self.A.copy()
+        Q = eye(self.m)
+
+        for i in range(self.n):
+            
+            if i == self.m-1:
+                break
+
+            G = eye(self.m)
+            x1 = R[i, -2]
+            x2 = R[i, -1]
+            r = np.sqrt(x1**2+x2**2)
+            c = x1/r
+            s = x2/r
+            J = np.array([[c, s], [s, -c]])
+            G[-2:,-2:] = J
+            R[:,-2:] = np.dot(R[:,-2:], J.T)
+            if both:
+                Q[-2:,:] = np.dot(J, Q[-2:,:])
+
+            for j in range(3, self.m-i+1):
+                x1 = R[i, -j]
+                x2 = r
+                r = sqrt(x1**2+x2**2)
+                c = x1/r
+                s = x2/r
+                J = np.array([[c, s], [s, -c]])
+                R[i:,-j:-j+2] = np.dot(R[i:,-j:-j+2], J.T)
+                if both:
+                    G[-j:-j+2,-j:] = np.dot(J,G[-j:-j+2,-j:])
+                    Q[-j:-j+2,:] = np.dot(J,Q[-j:-j+2,:])
+
+            #if both:
+             #   Q = np.dot(G, Q)
+
+            g_list.append(G)
+            #R = np.dot(R, G.T)
+
+        if not both:
+            return g_list, R.T
+
+        return Q.T, R.T
+                
---
+++ assignments/assignment2/testOrth.py
@@ -0,0 +1,137 @@
+from orthogonalization import *
+import orthogonalization
+from scipy import linalg as la
+import time
+
+if False:
+    nlist = [1, 10, 100, 1000]
+    norms = []
+    i_divs = []
+    eigs = []
+    dets = []
+    tol = 1e-6
+    eps = 1e-7
+
+    for n in nlist:
+        A = np.vstack((np.ones(n), eps*np.eye(n))).T  
+        O = Orthogonalization(A, assume_li = True)
+        Q = O.gramschmidt()
+        QTQ = np.dot(Q,Q.T)
+        norms.append(np.linalg.norm(Q, ord=2))
+        i_divs.append(np.linalg.norm(QTQ-eye(n), ord=2))
+        dev = np.abs(np.linalg.eig(QTQ)[0]-ones(n))
+        eigs.append(dev[dev>tol])
+        dets.append(np.linalg.det(QTQ))
+
+    print norms, "\n\n", i_divs, "\n\n", eigs, "\n\n", dets
+
+if False:
+    nlist = [1, 10, 100, 1000]
+    norms = []
+    i_divs = []
+    eigs = []
+    dets = []
+    tol = 1e-6
+    eps = 1e-7
+
+    for n in nlist:
+        A = np.vstack((np.ones(n), eps*np.eye(n))).T  
+        Q, r = la.qr(A)
+        QTQ = np.dot(Q,Q.T)
+        norms.append(np.linalg.norm(Q, ord=2))
+        i_divs.append(np.linalg.norm(QTQ-eye(n), ord=2))
+        dev = np.abs(np.linalg.eig(QTQ)[0]-ones(n))
+        eigs.append(dev[dev>tol])
+        dets.append(np.linalg.det(QTQ))
+
+    print norms, "\n\n", i_divs, "\n\n", eigs, "\n\n", dets
+
+if False:
+    n = 500
+    A = np.random.rand(n, n)
+    b = np.random.rand(n)
+    t0 = time.time() 
+    x = la.solve(A, b)
+    print time.time()-t0
+    print np.linalg.norm(dot(A, x)-b)
+
+    t0 = time.time()
+    Q, r = la.qr(A)
+    QTb = np.dot(Q.T, b)
+
+    x = zeros(n)
+
+    for i in range(n):
+        sum = 0
+
+        for k in range(n-i, n):
+            sum += r[n-i-1][k]*x[k]
+        
+        x[n-i-1] = (QTb[n-i-1]-sum)/r[n-i-1][n-i-1]
+    
+    print "\n", time.time()-t0
+    print np.linalg.norm(dot(A,x)-b)
+
+if False:
+    A = np.random.rand(5, 8)
+    o = Orthogonalization(A)
+    Q, R = o.householder()
+    print np.round(R, decimals = 3), "\n\n", np.round(np.dot(Q.T,Q), decimals = 3), "\n\n", np.round(o.A.T - np.dot(Q, R), decimals = 8) 
+
+
+if False:
+    nlist = [1, 10, 100, 1000]
+    norms = []
+    i_divs = []
+    eigs = []
+    dets = []
+    tol = 1e-6
+    eps = 1e-7
+
+    for n in nlist:
+        A = np.vstack((np.ones(n), eps*np.eye(n))).T  
+        O = Orthogonalization(A, assume_li = True)
+        Q, R = O.householder()
+        QTQ = np.dot(Q,Q.T)
+        norms.append(np.linalg.norm(Q, ord=2))
+        i_divs.append(np.linalg.norm(QTQ-eye(O.m), ord=2))
+        dev = np.abs(np.linalg.eig(QTQ)[0]-ones(O.m))
+        eigs.append(dev[dev>tol])
+        dets.append(np.linalg.det(QTQ))
+
+    print norms, "\n\n", i_divs, "\n\n", eigs, "\n\n", dets
+
+if False:
+    n = 500
+    A = np.random.rand(n+10, n)
+    o = Orthogonalization(A.T)
+    t0 = time.time()
+    Q, R = o.householder(both = True)
+    print "householder:\n", time.time()-t0, "\n"
+    t0 = time.time()
+    Q, R = la.qr(A)
+    print "scipy's qr:\n", time.time()-t0
+    
+
+if True:
+    A = np.random.rand(5, 5)
+    o = Orthogonalization(A)
+    Q, R = o.givens(both = True)
+    print np.round(R, decimals = 3), "\n\n", np.round(np.dot(Q.T,Q), decimals = 5), "\n\n", np.round(o.A.T - np.dot(Q, R), decimals = 8)     
+
+if True:
+    n = 750
+    A = np.random.rand(n, n)
+    o = Orthogonalization(A.T)
+    t0 = time.time()
+    Q, R = o.householder(both = True)
+    print "householder:\n", time.time()-t0, "\n"
+    t0 = time.time()
+    Q, R = o.givens(both = True)
+    print "givens:\n", time.time()-t0
+
+if False:
+    A = np.random.rand(5,8)
+    o = Orthogonalization(A)
+    q_list, R = o.givens(both = False)
+    print np.round(q_list, decimals = 3)

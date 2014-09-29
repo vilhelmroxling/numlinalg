33c092b1cd0c9ebcc639389443a1e12b00ee4ba8
atf10ago <axel.goteman@student.lu.se>
2014-09-29 14:31:42 +0200

    task4 (called 3)

--- assignments/assignment4/task2.py
+++ assignments/assignment4/task2.py
@@ -9,3 +9,15 @@ Created on Fri Sep 26 14:01:40 2014
 from __future__ import division
 from scipy import *
 from matplotlib.pyplot import *
+
+close("all")
+
+def p(x):
+    return x**9-18*x**8+144*x**7-672*x**6+2016*x**5-4032*x**4+5376*x**3-4608*x**2+2304*x-512
+
+def p_(x):
+    return (x-2)**9
+
+x = linspace(1.92, 2.08, 161)
+
+plot(x,p_(x))
---
+++ assignments/assignment4/task3.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 29 13:24:37 2014
+
+@author: Axel
+"""
+
+
+from __future__ import division
+from scipy import *
+from scipy import linalg
+from matplotlib.pyplot import *
+import numpy as np
+
+beta = 1
+alpha = 1
+size = 20
+#
+#A = random.rand(size,size)
+#Ainv = np.linalg.inv(A)
+
+A = linalg.hilbert(size)
+Ainv = linalg.invhilbert(size)
+
+u,s,v = np.linalg.svd(A)
+
+
+b = u[:,0]*alpha
+db = u[:,-1]*beta
+
+x = np.linalg.solve(A,b)
+xh = np.linalg.solve(A,b + db)
+
+
+
+dx = xh-x
+
+k = np.linalg.norm(Ainv,ord=2)*np.linalg.norm(A,ord=2)
+print k
+
+vl = np.linalg.norm(dx, ord = 2)/np.linalg.norm(x, ord=2)
+
+hl = (np.linalg.norm(db,ord=2)/np.linalg.norm(b,ord=2))
+print vl/hl
+
